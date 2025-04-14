1c803fbb3fdef597c8ff5517e30d76f5c5ade8f6
linxmedaille <96383313+linxmedaille@users.noreply.github.com>
2025-04-14 22:15:09 +0100

    push

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.Rendering;
+using TMPro;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
@@ -8,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float jumpHeight;
     private bool isHorisontalPressed = false;
     private bool isJumpPressed = false;
+    private bool isInTheAir = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,23 +26,36 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         moveLeft();
+        jump();
     }
 
     public void moveLeft()
     {
         if (isHorisontalPressed)
         {
-            rb.linearVelocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, 0);
+            Vector3 currentVelocity = rb.linearVelocity;
+            rb.linearVelocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, currentVelocity.y, 0);
+
         }
 
     }
 
-public void jump()
+    public void jump()
     {
-        if (isJumpPressed)
-        { 
-            rb.linearVelocity = new Vector3(0,0, Input.GetAxis("Jump") * moveSpeed);
+        if (isJumpPressed && !isInTheAir)
+        {
+            Vector3 currentVelocity = rb.linearVelocity;
+            rb.AddForce(new Vector3(currentVelocity.x, jumpHeight, 0), ForceMode.Impulse);
+            isInTheAir = true;
         }
 
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (isInTheAir == true)
+        {
+            isInTheAir = false;
+            rb.linearVelocity = Vector3.zero;
+        }
     }
 }
