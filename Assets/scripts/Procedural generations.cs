a721af4a2752eaab607821a2b0e46d424e0116d9
linxmedaille <linxmedaille@gmail.com>
2025-07-16 02:15:16 +0200

    first port settings

--- Assets/scripts/Procedural generations.cs
+++ Assets/scripts/Procedural generations.cs
@@ -1,10 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor.Rendering;
 using UnityEngine;
-using UnityEngine.InputSystem.Android;
 
 public class MeshGenerator : MonoBehaviour
 {
